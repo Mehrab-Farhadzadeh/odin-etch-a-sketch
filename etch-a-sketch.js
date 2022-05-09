a0e37de9f47145e9379264cbc982e30467e5326d
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2022-05-09 15:01:09 +0430

    Add getSquaresPerSideAndRebuildTheGrid function
    Add event listener to "Set Density" button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,30 +1,26 @@
-const MAX_ROW = getSquaresPerSide();
-const MAX_COL = MAX_ROW;
+let MAX_ROW;
+let MAX_COL;
 
-function createGrid(rowSize, colSize) {
+function createGrid(maxRow, maxCol) {
   const container = document.querySelector("div.container");
-  for (let row = 0; row < MAX_ROW; row++) {
+  for (let row = 0; row < maxRow; row++) {
     const rowContainerDiv = document.createElement("div");
     rowContainerDiv.classList.add("row");
     rowContainerDiv.classList.add(`${row}`);
-    for (let col = 0; col < MAX_COL; col++) {
+    for (let col = 0; col < maxCol; col++) {
       const colDiv = document.createElement("div");
       colDiv.classList.add("col");
       colDiv.classList.add(`${col}`);
-      setDimensionOfSquareDiv(colDiv, container);
+      setDimensionsOfSquareDiv(colDiv, container);
       rowContainerDiv.appendChild(colDiv);
     }
     container.appendChild(rowContainerDiv);
   }
 }
 
-function getSquaresPerSide() {
-  return prompt("Enter the number of squares per side? (MAX: 100)");
-}
-
-function setDimensionOfSquareDiv(squareDivNode, container) {
-  const canvasWidth = window.getComputedStyle(container).width.slice(0, -2);
-  const canvasHeight = window.getComputedStyle(container).height.slice(0, -2);
+function setDimensionsOfSquareDiv(squareDivNode, canvas) {
+  const canvasWidth = window.getComputedStyle(canvas).width.slice(0, -2);
+  const canvasHeight = window.getComputedStyle(canvas).height.slice(0, -2);
   squareDivNode.style.width = `${canvasWidth / MAX_COL}px`;
   squareDivNode.style.height = `${canvasHeight / MAX_ROW}px`;
 }
@@ -36,9 +32,33 @@ function addEventListenerToGridSquares(eventName, callback) {
   });
 }
 
-function addHoverClassToEvent(event) {
-  event.target.classList.add("mouseover");
+function removePreviousGrid() {
+  const body = document.querySelector("body");
+  body.removeChild(body.firstElementChild);
+  const container = document.createElement("div");
+  container.classList.add("container");
+  body.insertBefore(container, body.firstElementChild);
 }
 
-createGrid(MAX_ROW, MAX_COL);
-addEventListenerToGridSquares("mouseover", addHoverClassToEvent);
+function getSquaresPerSideAndRebuildTheGrid() {
+  MAX_ROW = MAX_COL = prompt(
+    "Enter the number of squares per side? (MAX: 100)"
+  );
+  if (MAX_ROW > 100) MAX_ROW = MAX_COL = 100;
+  removePreviousGrid();
+  createGrid(MAX_ROW, MAX_COL);
+  addEventListenerToGridSquares("mouseover", (event) =>
+    event.target.classList.add("mouseover")
+  );
+}
+
+function addClickEventToButton(buttonNode, callback) {
+  buttonNode.addEventListener("click", callback);
+}
+
+addClickEventToButton(
+  document.querySelector("button.setDensity"),
+  getSquaresPerSideAndRebuildTheGrid
+);
+
+// getSquaresPerSideAndRebuildTheGrid();
