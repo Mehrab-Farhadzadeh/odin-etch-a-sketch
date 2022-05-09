2b34e1c180a8a0e4955861b28359bc289ec907af
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2022-05-10 01:28:03 +0430

    Refactoring: Reorder the functions declarations
    Increase readability

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,3 +1,9 @@
+function setDimensionsOfSquareDiv(squareDivNode, canvas, maxRow, maxCol) {
+  const canvasWidth = window.getComputedStyle(canvas).width.slice(0, -2);
+  const canvasHeight = window.getComputedStyle(canvas).height.slice(0, -2);
+  squareDivNode.style.width = `${canvasWidth / maxCol}px`;
+  squareDivNode.style.height = `${canvasHeight / maxRow}px`;
+}
 function createGrid(maxRow, maxCol) {
   const container = document.querySelector("div.container");
   for (let row = 0; row < maxRow; row++) {
@@ -15,13 +21,6 @@ function createGrid(maxRow, maxCol) {
   }
 }
 
-function setDimensionsOfSquareDiv(squareDivNode, canvas, maxRow, maxCol) {
-  const canvasWidth = window.getComputedStyle(canvas).width.slice(0, -2);
-  const canvasHeight = window.getComputedStyle(canvas).height.slice(0, -2);
-  squareDivNode.style.width = `${canvasWidth / maxCol}px`;
-  squareDivNode.style.height = `${canvasHeight / maxRow}px`;
-}
-
 function addEventListenerToGridSquares(eventName, callback) {
   const squares = document.querySelectorAll("div.col");
   squares.forEach((square) => {
@@ -35,6 +34,19 @@ function removeEventListenerFromGridSquares(eventName, callback) {
   });
 }
 
+function increaseBgColorOpacity(event = new MouseEvent()) {
+  const currentBgColor = window.getComputedStyle(event.target).backgroundColor;
+  if (!currentBgColor.includes("rgba")) return;
+  const currentOpacity = currentBgColor.slice(-4, -1);
+  event.target.style.backgroundColor = `${currentBgColor.slice(0, -4)}${
+    +currentOpacity + 0.1
+  })`;
+}
+function createAnEtchASketch(maxRow, maxCol) {
+  createGrid(maxRow, maxCol);
+  addEventListenerToGridSquares("mouseover", increaseBgColorOpacity);
+}
+
 function removePreviousGrid() {
   const body = document.querySelector("body");
   body.removeChild(body.firstElementChild);
@@ -42,7 +54,6 @@ function removePreviousGrid() {
   container.classList.add("container");
   body.insertBefore(container, body.firstElementChild);
 }
-
 function getSquaresPerSideAndRebuildTheCanvas() {
   let MAX_ROW;
   let MAX_COL;
@@ -52,24 +63,6 @@ function getSquaresPerSideAndRebuildTheCanvas() {
   if (MAX_ROW > 100) MAX_ROW = MAX_COL = 100;
   removePreviousGrid();
   createAnEtchASketch(MAX_ROW, MAX_COL);
-}
-
-function addClickEventToButton(buttonNode, callback) {
-  buttonNode.addEventListener("click", callback);
-}
-
-function increaseBgColorOpacity(event = new MouseEvent()) {
-  const currentBgColor = window.getComputedStyle(event.target).backgroundColor;
-  if (!currentBgColor.includes("rgba")) return;
-  const currentOpacity = currentBgColor.slice(-4, -1);
-  event.target.style.backgroundColor = `${currentBgColor.slice(0, -4)}${
-    +currentOpacity + 0.1
-  })`;
-}
-
-function createAnEtchASketch(maxRow, maxCol) {
-  createGrid(maxRow, maxCol);
-  addEventListenerToGridSquares("mouseover", increaseBgColorOpacity);
 }
 
 function generateARandomRGB() {
@@ -92,14 +85,16 @@ function toggleRainbowMode(event) {
   event.target.classList.toggle("active");
 }
 
+function addClickEventToButton(buttonNode, callback) {
+  buttonNode.addEventListener("click", callback);
+}
+
 addClickEventToButton(
   document.querySelector("button.setDensity"),
   getSquaresPerSideAndRebuildTheCanvas
 );
-
 addClickEventToButton(
   document.querySelector("button.rainbowMode"),
   toggleRainbowMode
 );
-
 createAnEtchASketch(16, 16);
